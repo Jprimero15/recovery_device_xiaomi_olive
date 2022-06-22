518eb9a21c98646abca0df7dd75893d63d051cc8
Jprimero15 <jprimero155@gmail.com>
2022-06-22 10:22:22 +0800

    olive: add core_64_bit_only inherit-product
    * lets be clear that this recovery is 64bit only
    
    Signed-off-by: Jprimero15 <jprimero155@gmail.com>

--- device.mk
+++ device.mk
@@ -16,6 +16,7 @@
 
 # Inherit from common AOSP config
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Dependecies
